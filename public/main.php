17cdebcc5960e3fbc5d7b720c261d27889292a5b
Fedor Shurcov <fedorshurcov1234@mail.ru>
2022-12-13 02:50:48 +0300

    log secret

--- public/main.php
+++ public/main.php
@@ -10,6 +10,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $log = new Logger('logger');
 $log->pushHandler(new StreamHandler('../log.log'));
 $log->debug('aboba');
-$handler = new ServerHandler();
+$handler = new ServerHandler($log);
 $data = json_decode(file_get_contents('php://input'));
 $handler->parse($data);
--- src/Controllers/ServerHandler.php
+++ src/Controllers/ServerHandler.php
@@ -11,11 +11,14 @@ class ServerHandler extends VKCallbackApiServerHandler
     const SECRET = 'vk1.a._yZ8p18plSQOmZiPgtYyeO2muWSzEC_ODtMLqaBdCJNHAmkvazycnE2d42YMRaKbOSKb-kxYNsOOD_LcvQhzM_WjJKvNrYwacJF1NhToBYyd3gFnxyW9bMfjOVmCK8hHSi86BBzDEqHz3eniRoE1vqfYZLV9rdV9lM07yrx2YB8KJkxfiO2-vOPAMPDI7vW-zzYzf5RDDNv0MtU-lrmA_w';
     const GROUP_ID = 217490194;
     const CONFIRMATION_TOKEN = 'e5f0bdff';
+    public $log;
+
+    function __construct($log) {
+        $this->log = $log;
+    }
 
     function confirmation(int $group_id, ?string $secret) {
-        if ($secret === null) {
-            echo "lol";
-        }
+        $this->log->debug($secret);
         if ($secret === static::SECRET && $group_id === static::GROUP_ID) {
             echo static::CONFIRMATION_TOKEN;
         }
