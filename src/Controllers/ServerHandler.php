4903c0f73eb0e1255a425b4ec2b67c6c7351bbe5
Fedor Shurcov <fedorshurcov1234@mail.ru>
2022-12-13 03:07:41 +0300

    confirmation token

--- src/Controllers/ServerHandler.php
+++ src/Controllers/ServerHandler.php
@@ -10,7 +10,7 @@ class ServerHandler extends VKCallbackApiServerHandler
 {
     const SECRET = 'vk1.a._yZ8p18plSQOmZiPgtYyeO2muWSzEC_ODtMLqaBdCJNHAmkvazycnE2d42YMRaKbOSKb-kxYNsOOD_LcvQhzM_WjJKvNrYwacJF1NhToBYyd3gFnxyW9bMfjOVmCK8hHSi86BBzDEqHz3eniRoE1vqfYZLV9rdV9lM07yrx2YB8KJkxfiO2-vOPAMPDI7vW-zzYzf5RDDNv0MtU-lrmA_w';
     const GROUP_ID = 217490194;
-    const CONFIRMATION_TOKEN = 'e5f0bdff';
+    const CONFIRMATION_TOKEN = 'd63edc31';
     public $log;
 
     function __construct($log) {
