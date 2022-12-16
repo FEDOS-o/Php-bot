c9e0228bd4b095180b8f3801b134d27ba05bea19
Fedor Shurcov <fedorshurcov1234@mail.ru>
2022-12-16 16:15:50 +0300

    updated conf token

--- src/Controllers/ServerHandler.php
+++ src/Controllers/ServerHandler.php
@@ -2,7 +2,11 @@
 
 namespace App\Controllers;
 
+use App\Services\DialogueDataBaseService;
+use App\Services\VkApiService;
+use Monolog\Logger;
 use VK\CallbackApi\Server\VKCallbackApiServerHandler;
+use VK\Client\VKApiClient;
 
 require __DIR__ . '/../../vendor/autoload.php';
 
@@ -11,22 +15,27 @@ class ServerHandler extends VKCallbackApiServerHandler
     const TOKEN = 'vk1.a._yZ8p18plSQOmZiPgtYyeO2muWSzEC_ODtMLqaBdCJNHAmkvazycnE2d42YMRaKbOSKb-kxYNsOOD_LcvQhzM_WjJKvNrYwacJF1NhToBYyd3gFnxyW9bMfjOVmCK8hHSi86BBzDEqHz3eniRoE1vqfYZLV9rdV9lM07yrx2YB8KJkxfiO2-vOPAMPDI7vW-zzYzf5RDDNv0MtU-lrmA_w';
     const SECRET = 'aaQ1fsdf9ornzqwemc';
     const GROUP_ID = 217490194;
-    const CONFIRMATION_TOKEN = 'd63edc31';
-    public $log;
+    const CONFIRMATION_TOKEN = '1b52fd41';
+    private Logger $logger;
+    private DialogueDataBaseService $db;
+    private VKApiService $vk;
 
-    function __construct($log)
+
+    function __construct($logger, $connection, $vkApi)
     {
-        $this->log = $log;
+        $this->logger = $logger;
+        $this->db = new DialogueDataBaseService($connection);
+        $this->vk = new VkApiService($vkApi);
     }
 
     function confirmation(int $group_id, ?string $secret)
     {
         if ($secret !== static::SECRET) {
-            $this->log->debug('secret key is invalid');
+            $this->logger->debug('secret key is invalid');
             return;
         }
         if ($group_id !== static::GROUP_ID) {
-            $this->log->debug('group id is invalid');
+            $this->logger->debug('group id is invalid');
             return;
         }
         echo static::CONFIRMATION_TOKEN;
@@ -41,12 +50,12 @@ class ServerHandler extends VKCallbackApiServerHandler
             $secret = $event->secret;
             $type = $event->type;
             $object = (array)$event->object;
-            $this->log->debug("Received message of type $type from group $group_id: " . json_encode($object));
+            $this->logger->debug("Received message of type $type from group $group_id: " . json_encode($object));
             if (
                 $secret !== static::SECRET ||
                 $group_id !== static::GROUP_ID
             ) {
-                $this->log->debug("Secret key or group id is invalid");
+                $this->logger->debug("Secret key or group id is invalid");
                 return;
             }
             $this->parseObject($group_id, $secret, $type, $object);
@@ -55,6 +64,15 @@ class ServerHandler extends VKCallbackApiServerHandler
 
     public function messageNew(int $group_id, ?string $secret, array $object)
     {
+        $chat_id = $object['peer_id'];
+        $status = $this->db->get_status($chat_id);
+        switch ($status) {
+            case 0:
+                break;
+            case 1;
+                break;
+
+        }
         echo 'ok';
     }
 }
