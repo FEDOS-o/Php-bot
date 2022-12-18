12d69fcbef0535e38e1e56a61740453c9b62ca63
Fedor Shurcov <fedorshurcov1234@mail.ru>
2022-12-18 15:22:59 +0300

    add empty constructor

--- src/Services/RandomFilmService.php
+++ src/Services/RandomFilmService.php
@@ -8,7 +8,11 @@ use GuzzleHttp\Client;
 class RandomFilmService
 {
 
-    public function get_films($params) {
+    function __construct() {
+
+    }
+
+    public function get_films(FilmRequest $params) {
         $client = new Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false, ), ));
         $response = $client->request('GET', 'https://www.kinopoisk.ru/chance/?item=true&not_show_rated=false&count=' . $params->count . '&max_years=' . $params->max_years . '&min_years=' . $params->min_years . '&rnd=' . $this->random01());
         $result = json_decode($response->getBody(), true);
